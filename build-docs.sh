413db7d79252b2d5bfd2f985980c8e54309efcdb
Stefan Wallentowitz <stefan@wallentowitz.de>
2015-12-29 19:43:57 +0100

    Update build-docs.sh to unified tree

--- build-docs.sh
+++ build-docs.sh
@@ -3,8 +3,8 @@
 # Build the OpTiMSoC web page
 #
 
-OPTIMSOC_DOCS_REPO_URL=${1:-https://github.com/optimsoc/docs.git}
-OPTIMSOC_DOCS_BRANCH=${2:-master}
+OPTIMSOC_REPO_URL=${1:-https://github.com/optimsoc/sources.git}
+OPTIMSOC_BRANCH=${2:-master}
 
 # switch to script directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -14,12 +14,11 @@ rm -rf buildtmp buildout
 mkdir buildtmp
 
 # get sources
-git clone $OPTIMSOC_DOCS_REPO_URL buildtmp/optimsoc-docs
-( cd buildtmp/optimsoc-docs; git checkout $OPTIMSOC_DOCS_BRANCH )
+git clone -b $OPTIMSOC_BRANCH $OPTIMSOC_REPO_URL buildtmp/optimsoc
 
 # User guide: HTML
 mkdir -p buildtmp/user_guide/{tex,xml,html}
-cp buildtmp/optimsoc-docs/user_guide/* buildtmp/user_guide/tex
+cp buildtmp/optimsoc/doc/user_guide/* buildtmp/user_guide/tex
 cp docbuild/user_guide-html.tex buildtmp/user_guide/tex
 
 latexml --dest=buildtmp/user_guide/xml/user_guide.xml \
@@ -34,12 +33,12 @@ cp -r buildtmp/user_guide/html/*.html docs/user-guide
 test -d buildtmp/user_guide/html/mi && cp -r buildtmp/user_guide/html/mi docs/user-guide
 
 # User guide: PDF
-( cd buildtmp/optimsoc-docs/user_guide; pdflatex user_guide.tex && pdflatex user_guide.tex )
-cp buildtmp/optimsoc-docs/user_guide/user_guide.pdf docs/user-guide.pdf
+( cd buildtmp/optimsoc/doc/user_guide; pdflatex user_guide.tex && pdflatex user_guide.tex )
+cp buildtmp/optimsoc/doc/user_guide/user_guide.pdf docs/user-guide.pdf
 
 # Reference Manual
 mkdir -p buildtmp/refman/{tex,xml,html}
-cp buildtmp/optimsoc-docs/refman/* buildtmp/refman/tex
+cp buildtmp/optimsoc/doc/refman/* buildtmp/refman/tex
 cp docbuild/refman-*.tex buildtmp/refman/tex
 
 latexml --dest=buildtmp/refman/xml/refman.xml \
